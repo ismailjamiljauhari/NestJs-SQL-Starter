d7e49249f364ed3fb773af6615dc88e2eead82bf
Ismail Jamil Jauhari <ismailjamiljauhari@gmail.com>
2022-04-08 19:17:45 +0700

    tmp login with google

--- config/auth.ts
+++ config/auth.ts
@@ -3,8 +3,12 @@ dotenv.config()
 
 const JWT_SECRET: string = process.env.JWT_SECRET || 'jwtSecret'
 const JWT_EXPIRE: string = process.env.JWT_EXPIRE || '7d'
+const GOOGLE_CLIENT_ID: string = process.env.GOOGLE_CLIENT_ID || '930891391604 - tgk3983d0l4rf6a17djfs4ucubdcif7g.apps.googleusercontent.com'
+const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET || 'GOCSPX - iLP9Y39dyat4vltxBEGrthyBrIqU'
 
 export {
     JWT_SECRET,
     JWT_EXPIRE,
+    GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET,
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, UseGuards, Post, Request, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Request, Get, Param, NotFoundException, Body, HttpException, HttpStatus } from '@nestjs/common';
 
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from 'config/auth';
 import { AuthService } from './auth.service';
+import { LoginSocialiteDTO } from './dto/login-socialite.dto';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
@@ -19,5 +21,39 @@ export class AuthController {
     @Get('profile')
     async getCurrentUserFromJWT(@Request() req) {
         return this.authService.getCurrentUser(req.user.username);
+    }
+
+    @Post('loginb/:provider')
+    async loginWithSocialite(
+        @Param('provider') provider, 
+        @Body() payload: LoginSocialiteDTO,
+        @Request() req
+    ){
+        const token = payload.token
+        let userPayload = null
+        if (provider == 'google') {
+            try {
+                const { OAuth2Client } = require('google-auth-library')
+                const client = new OAuth2Client(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET)
+                const ticket = await client.verifyIdToken({
+                    idToken: token,
+                    requiredAudience: GOOGLE_CLIENT_ID,
+                });
+                userPayload = ticket.getPayload()
+            } catch (error) {
+                throw new HttpException({
+                    statusCode: HttpStatus.BAD_REQUEST,
+                    message: 'Ada masalah saat autentikasi, silahkan coba lagi!',
+                }, HttpStatus.BAD_REQUEST);
+            }
+        }
+        else {
+            throw new HttpException({
+                statusCode: HttpStatus.BAD_REQUEST,
+                message: 'Ada masalah saat autentikasi, silahkan coba lagi!',
+            }, HttpStatus.BAD_REQUEST);
+        }
+        
+        return userPayload
     }
 }
